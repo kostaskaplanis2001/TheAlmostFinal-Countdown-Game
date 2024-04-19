99b3f8a41ac7729113eadcfa1d15bf5df14ab619
Kostas Kaplanis <kostaskaplanis2001@hotmail.com>
2024-04-19 13:16:38 +0300

    Forwarding Ref to Custom Component

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -5,7 +5,7 @@ import ResultModal from './ResultModal.jsx';
 
 export default function TimerChallenge({ title, targetTime }) {
     const timer = useRef();
-
+    const dialog = useRef();
 
     const [timerStarted, setTimerStarted] = useState(false);
     const [timerExpired, setTimerExpired] = useState(false);
@@ -14,6 +14,7 @@ export default function TimerChallenge({ title, targetTime }) {
     function handleStart() {
         timer.current = setTimeout(() => {
             setTimerExpired(true);
+            dialog.current.showModal();
         }, targetTime * 1000);
 
         setTimerStarted(true);
@@ -25,7 +26,7 @@ export default function TimerChallenge({ title, targetTime }) {
 
     return (
         <>
-            {timerExpired && <ResultModal targetTime={targetTime} result="lost" />}
+            <ResultModal ref={dialog} targetTime={targetTime} result="lost" />
             <section className="challenge">
                 <h2>{title}</h2>
                 {timerExpired && <p>You lost!</p>}
